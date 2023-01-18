9593c405432cc9d3baec1ccd766a74b6bc57b88d
Igor Abreu <igor29nahan@gmail.com>
2023-01-18 18:00:45 -0300

    Finalizando o projeto de botões

---
+++ index.js
@@ -0,0 +1,9 @@
+const buttons = document.getElementById("buttonInteract")
+
+function openBoxButtons() {
+  buttons.style.opacity = "1"
+}
+
+function closeBoxButtons() {
+  buttons.style.opacity = "0"
+}
